7ec65dbb34ebe584e80ba1d94b5c3b8ffc0d23d3
Bascou Herceg <tunombre@email.com>
2025-11-24 15:19:23 -0300

    Python requirements

---
+++ python/params.py
@@ -0,0 +1,5 @@
+# Cinematica
+
+# angulos homing: angulos absolutos a los que queda el brazo al hacer homing
+homing_angle_1 = 80.03  # desde horizontal hacia arriba
+homing_angle_2 = 64.95  # desde eslabon 1 a eslabon 2   
